e7788c9b32a62ae66618c89a7545538eac9f451a
dsc381 <dsc381@nyu.edu>
2013-09-28 20:38:50 -0400

    Changed tbbay to functions

--- tbbay.py
+++ tbbay.py
@@ -11,17 +11,25 @@ def read(file_of_words):
         corpus_words += i
     return corpus_words
 
-incoming = sys.argv[1]
-incoming_crazy = sys.argv[2]
-normal = TextBlob(read(incoming))
-crazy = TextBlob(read(incoming_crazy))
-normal.sentences
-crazy.sentences
+def tokenize(normal, crazy):
+#    incoming = sys.argv[1]
+#    incoming_crazy = sys.argv[2]
+#    normal = TextBlob(read(incoming))
+#    crazy = TextBlob(read(incoming_crazy))
+    return normal.sentences, crazy.sentences
 
-train = []
-for sentences in normal:
-    train += (sentences, 'pos')
-for sentences in normal:
-    train += (sentences, 'neg')
 
-bayes = NaiveBayesClassifier(train)
+
+def train(normal, crazy):
+    train = []
+    for sentences in normal:
+        train += (sentences, 'pos')
+    for sentences in crazy:
+        train += (sentences, 'neg')
+    bayes = NaiveBayesClassifier(train)
+    return bayes
+
+def classify(new_comment, bayes):
+    '''accepts string'''
+    analyze = TextBlob(new_comment,classifier = bayes)
+    return analyze.classify()
