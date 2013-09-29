4f20198ae5d009fbf15263441b8ff659e3eb4b20
chris williams <chilliams@gmail.com>
2013-09-28 20:55:56 -0400

    hacky mongo stuff

---
+++ sanemongo.py
@@ -0,0 +1,13 @@
+from pymongo import MongoClient
+import json
+
+client = MongoClient()
+db = client.comments
+
+comments = db.sanepeople.find({}, { "results.comments.commentBody": 1})
+
+for entry in comments:
+    if entry.has_key("results"):
+        if entry.get("results").has_key("comments"):
+            for comment in entry.get("results").get("comments"):
+                print comment.get("commentBody")
